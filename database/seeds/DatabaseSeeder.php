1d5871ce8f980cc3bf32db2ab3318ac87b1cd48b
Freek <freekwernas2@hotmail.com>
2016-05-13 20:31:32 +0200

    Mijn commitje 3 hoop ik

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -32,6 +32,9 @@ class DatabaseSeeder extends Seeder
                 'user_id' => '1',
             ],
             [
+                'user_id' => '1',
+            ],
+            [
             'user_id' => '1',
         ]]);
 
